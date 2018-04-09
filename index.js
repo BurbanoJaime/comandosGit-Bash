1119c49c2f559f96e7e20d31bcc90316268e4891
Jaime Burbano <burbanojaime98@gmail.com>
2018-04-09 08:47:45 -0500

    comandos

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 var emoji = require('node-emoji');
 console.console.log(emoji.get('coffe'));
+
+
+
+
+/*
+npm init : Inicializa el npm
+git init: Inicializa el git
+
+npm i -libreria-
+npm instal -libreria- : Instala las librerias
+
+git rm -f : elimina de git y de la carpeta
+git rm -fr : Elimina solo de git
+
+git status: muestra el estado de lso archivos
+
+git log : muestra los commit
+git branch - nombre- : crea un nuevo repositorio o "carpeta", normalmente se usa para trabajos experimentales o que apenas se inician
+git checkout -nombre-: Cambia de repositorio al repositorio -nombre-
+
+git add . : agrega todos los archivos que no estan ignorados al git
+git add -nombre-: agrega el archivo -nombre- al git
+git commit -m "mensaje" : agrega un commit
+
+git clone : clona un repositorio
+
+git reset --had -nombreDelCommit- : regresa el codigo hasta el commit -nombreDelCommit- //EL CAMBIO ES IRREVERSIBLE
+
+git remote add -nombredelRemoto- https://github.com/usuario/repositorio.git : Conecta el codigo al github de internet
+
+
+//PRIMER PUSH
+git push -u -nombredelRemoto branch : Sube el codigo al repositorio de internet
+/La U significa 
+//nombredelRemoto, generalmente es "origin"
+
+*/
